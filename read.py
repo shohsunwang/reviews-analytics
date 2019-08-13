0b1864f2947b475076ac529770b1a8d401f7e766
Rick Wang <sho.hsun.wang@gmail.com>
2019-08-13 22:33:05 +0800

    add fittering

--- read.py
+++ read.py
@@ -4,7 +4,7 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count +=1  #count = count + 1
-        if count % 100000 ==0:
+        if count % 1000 ==0:
             print(len(data))
 print('檔案讀取完了總共有', len(data), '筆資料')
 print(len(data[0]))
@@ -17,6 +17,12 @@ for d in data:
 
 print('留言的平均長度為', sum_len/len(data))
 
-
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
 
 
