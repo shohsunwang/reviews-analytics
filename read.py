2f533f43ee87cd2f2bf73a2d11a085461f26409d
Rick Wang <sho.hsun.wang@gmail.com>
2019-08-13 22:38:46 +0800

    add mentioned good fitering

--- read.py
+++ read.py
@@ -25,4 +25,13 @@ print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
 
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
+print(good[1])
+
+
 
